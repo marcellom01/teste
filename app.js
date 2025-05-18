03d2d80b35542246ba07cdee159ff2eebc58fded
Marcello Miranda <marcello.sbs@gmail.com>
2025-05-18 20:31:25 -0300

    porta alterada

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // Definindo a porta onde o servidor irá escutar
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Rota principal que retorna "Olá, Mundo!"
 app.get('/', (req, res) => {
